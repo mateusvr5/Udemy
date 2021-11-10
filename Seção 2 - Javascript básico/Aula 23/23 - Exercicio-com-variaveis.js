88639511e7c2cef327ddf00146e40c3de186751a
mateusvr5 <mateusvr5@gmail.com>
2021-11-10 11:49:24 -0300

    Udemy

--- Seção 2 - Javascript básico/Aula 23/23 - Exercicio-com-variaveis.js
+++ Seção 2 - Javascript básico/Aula 23/23 - Exercicio-com-variaveis.js
@@ -3,10 +3,14 @@ let varB = 'B'; // C
 let varC = 'C'; // A
 
 const aux = varA;
-
+/*
 varA = varB;
 varB = varC;
 varC = aux;
+*/
+//Outra maneira de resolução
+
+[varA, varB, varC] = [varB, varC, varA];
 
 
 console.log(varA, varB, varC);
---
+++ Seção 2 - Javascript básico/Aula 24/24 - Mais-sobre-strings.js
@@ -0,0 +1,6 @@
+//let umaString = "Um \"texto\"";
+//               01234567 caracteres
+let umaString = "Um texto";
+
+console.log(umaString[4]);
+console.log(umaString[8]); // Caractere indefinido na String
