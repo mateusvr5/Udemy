08515d58b02cc08862598a91ebdbdbb6f54a9070
mateusvr5 <mateusvr5@gmail.com>
2021-11-22 11:39:51 -0300

    Udemy

---
+++ Seção 3 - Lógica de programação/Aula 37/37-operadores-de-comparacao.js
@@ -0,0 +1,30 @@
+/*
+Operadores de comparação
+> Maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) //evitar
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 10 > 5;
+console.log(comp);
+
+const comp2 = 10 >= 11;
+console.log(comp2);
+
+const comp3 = 10 < 11;
+console.log(comp3);
+
+const comp4 = 12 <= 11;
+console.log(comp4);
+
+const comp5 = 10 === 10;
+console.log(comp5);
+
+const comp6 = 10 !== 10;
+console.log(comp6);
+
