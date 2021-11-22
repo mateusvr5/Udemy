77bb8eb710b7a7e6e19ef090428e0916b23785eb
mateusvr5 <mateusvr5@gmail.com>
2021-11-22 09:27:10 -0300

    Udemy

---
+++ Seção 2 - Javascript básico/Aula34/js/script.js
@@ -0,0 +1,45 @@
+// IIFE
+function meuEscopo (){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+
+       const nome = form.querySelector('.nome');
+       const sobrenome = form.querySelector('.sobrenome');
+       const peso = form.querySelector('.peso');
+       const altura = form.querySelector('.altura');
+
+
+    
+
+       pessoas.push({
+         nome: nome.value,
+         sobrenome: sobrenome.value,
+         peso: peso.value,
+         altura: altura.value
+
+       });
+
+       console.log(pessoas);
+
+       resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` + ` ${peso.value}  ${altura.value}</p>`;
+
+      }
+    
+
+      //Método mais antigo
+    // form.onsubmit = function(evento) {
+    //     evento.preventDefault();
+    //     alert('Dados enviados com sucesso');
+    //     console.log('Foi enviado');
+    // };
+
+    //Método mais atualizado
+    form.addEventListener('submit', recebeEventoForm);
+
+}
+meuEscopo();
