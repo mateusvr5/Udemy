f59099c918608c1c95a11f58fc325444a31bdb94
mateusvr5 <mateusvr5@gmail.com>
2021-11-25 11:46:39 -0300

    Udemy

--- Seção 3 - Lógica de programação/Aula 46/46-objeto-date.js
+++ Seção 3 - Lógica de programação/Aula 46/46-objeto-date.js
@@ -2,14 +2,35 @@
 // const umDia = 60*60 *24 *1000;
 // const data = new Date(0 + tresHoras - umDia); // 01/01/1970 Timestamp unix ou época unix
 
-const data = new Date('2019-04-20 20:15:59.500'); //ano, mes, dia, hora(h), minuto(min), segundo(s), milésimo de segundo(ms)
-console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth() + 1); // Mês começa do zero
-console.log('Ano', data.getFullYear());
-console.log('Hora', data.getHours());
-console.log('Minuto', data.getMinutes());
-console.log('Segundo', data.getSeconds());
-console.log('Milisegundos', data.getMilliseconds());
-console.log('Dia da semana', data.getDay()); // 0 - Domingo, 6 - Sábado
-console.log(data.toString());
-console.log(Date.now());
+// const data = new Date('2019-04-20 20:15:59.500'); //ano, mes, dia, hora(h), minuto(min), segundo(s), milésimo de segundo(ms)
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth() + 1); // Mês começa do zero
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Minuto', data.getMinutes());
+// console.log('Segundo', data.getSeconds());
+// console.log('Milisegundos', data.getMilliseconds());
+// console.log('Dia da semana', data.getDay()); // 0 - Domingo, 6 - Sábado
+// console.log(data.toString());
+// console.log(Date.now());
+
+function zeroAEsquerda(num){
+    return num >= 10 ? num: `0${num}`
+}
+
+
+function formataData(data){
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+    const mili = zeroAEsquerda(data.getMilliseconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}:${mili}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
