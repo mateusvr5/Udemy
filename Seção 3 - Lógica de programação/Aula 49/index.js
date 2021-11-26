79de645bbb665434bc90383613f51308b3b9cc87
mateusvr5 <mateusvr5@gmail.com>
2021-11-26 11:34:57 -0300

    Udemy

---
+++ Seção 3 - Lógica de programação/Aula 49/index.js
@@ -0,0 +1,23 @@
+//const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Luiz'; //Criando
+// var nome2 = 'Luiz';
+
+// if(verdadeira){
+//     let nome = 'Otávio';//Criando
+//     console.log(nome, nome2);
+
+//     if(verdadeira) {
+//         console.log('Ok');
+//     }
+// }
+
+var sobrenome = 'Miranda';
+
+function falaOi () {
+    console.log(sobrenome);
+}
+falaOi();
