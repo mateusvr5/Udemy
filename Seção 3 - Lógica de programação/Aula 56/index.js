4ef1b176ba5a63638a535bf5b2c29faaec846a43
mateusvr5 <mateusvr5@gmail.com>
2021-12-02 08:15:13 -0300

    Udemy

--- Seção 3 - Lógica de programação/Aula 56/index.js
+++ Seção 3 - Lógica de programação/Aula 56/index.js
@@ -1,25 +1,34 @@
 //            0123...
-const nome = ['Luiz', 'Otávio', 'Henrique'];
+//const nome = ['Luiz', 'Otávio', 'Henrique'];
 
 // for(let i=0;i<nome.length;i++){
 //     console.log(nome[i]);
 // }
 
-console.log('####')
+//console.log('####')
 
-for(let i in nome){
-     console.log(nome[i]);
+//for(let i in nome){
+    // console.log(nome[i]);
      
-}
+//}
 
-console.log('####');
+//console.log('####');
 
-for (let valor of nome){
-    console.log(valor);
-}
+// for (let valor of nome){
+//     console.log(valor);
+// }
 
-console.log('####');
+// console.log('####');
 
-nome.forEach(function(valor, indice, array){
-    console.log(valor, indice, array);
-});
+// nome.forEach(function(valor, indice, array){
+//     console.log(valor, indice);
+// });
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+};
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
