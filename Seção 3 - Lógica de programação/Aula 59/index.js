45656199157bb4727f964cdae0c6e267c9a15728
Mateus Almeida <mateusvr5@gmail.com>
2021-12-02 11:29:18 -0300

    Udemy

--- Seção 3 - Lógica de programação/Aula 59/index.js
+++ Seção 3 - Lógica de programação/Aula 59/index.js
@@ -1,11 +1,15 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+// Continue - continua para próxima iteração
+// Break - Sai do laço
+
 let i = 0;
 while(i < numeros.length){
     let numero = numeros[i];
 
     if(numero === 2){
         console.log('Pulei o número 2');
+        i++;
         continue;
     }
 
