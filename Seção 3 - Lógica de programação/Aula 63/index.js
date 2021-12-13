df4821dc1c75914bf6373d45a2fb328e18ebd175
Mateus Almeida <mateusvr5@gmail.com>
2021-12-13 09:20:32 -0300

    Udemy

---
+++ Seção 3 - Lógica de programação/Aula 63/index.js
@@ -0,0 +1,26 @@
+// try{
+//     console.log(naoExisto);
+// } catch(error){
+//     console.log('NaoExisto não existe');
+
+// }
+
+
+function soma(x, y){
+    if(typeof x !== 'number' || 
+    typeof y !== 'number'
+    ) {
+        throw new TypeError('x e y precisam ser números.');
+    }
+
+    return x + y;
+}
+
+try{
+    console.log(soma(1,2));
+    console.log(soma('1',2));
+} catch(error){
+  console.log(error);
+  console.log('Alguma coisa mais amigável para o usuário')
+}
+
