e41f5bd1d4bdc049b9e65cf5d1c56abdcb8fca23
Mateus Almeida <mateusvr5@gmail.com>
2022-02-15 10:45:35 -0300

    Udemy

--- Seção 4 - Javascript Funções (Avançado)/Aula 75/main.js
+++ Seção 4 - Javascript Funções (Avançado)/Aula 75/main.js
@@ -1,35 +1,33 @@
-// Factory function (Função Fábrica)
-// Constructor funcion (função construtora)
-function criaPessoa(nome, sobrenome, altura, peso) {
+// Factory function (Função fábrica)
+// Constructor function (Função construtora)
+function criaPessoa(nome,sobrenome, altura, peso){
     return {
-        nome, 
+        nome,
         sobrenome,
-        // Getter
         get nomeCompleto(){
-            return `${this.nome} ${this.sobrenome} `
+            return `${this.nome} ${this.sobrenome}`;
         },
 
+        // Setter
         set nomeCompleto(valor){
             valor = valor.split(' ');
             this.nome = valor.shift();
             this.sobrenome = valor.join(' ');
+            console.log(valor);
         },
 
-        fala(assunto = 'Falando sobre NADA.') {
-            return `${this.nome} está ${assunto}`;
+        fala(assunto = 'falando sobre NADA.') {
+            return `${nome} ${sobrenome} está ${assunto}.`
         },
+
         altura: altura,
         peso: peso,
+
         // Getter
-        get imc() {
+        get imc(){
             const indice = this.peso / (this.altura ** 2);
             return indice.toFixed(2);
         }
     };
 }
-
-const p1 = criaPessoa('Luiz', 'Otávio',1.8, 80);
-p1.nomeCompleto = 'Maria Oliveira Silva';
-console.log(p1.nome);
-console.log(p1.sobrenome);
-console.log(p1.fala());
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
