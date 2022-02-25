4c57431ba4c56c370454caac85016ae940c8b121
Mateus Almeida <mateusvr5@gmail.com>
2022-02-25 09:14:53 -0300

    Udemy

---
+++ Seção 5 - JavaScript Arrays (Avançado)/Aula 82/index.js
@@ -0,0 +1,12 @@
+//
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(indice, delete, elem1, elem2, elem3 );
+//pop
+//const removidos = nomes.splice(-1, 1);
+
+// push
+nomes.splice(2, 0, 'Luiz', 'Otávio');
+console.log(nomes);
+
+
